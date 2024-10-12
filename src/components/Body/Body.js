7adf37e83ac33a1e3a4b2cadf3e1b3ab8cbe6cd7
vijay <vijayy2500@gmail.com>
2024-10-12 07:01:23 +0530

    Added menu data

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import { Img_url } from "../Contents";
 import Shimer from "../Shimer/Shimer";
 import "./Body.css";
+import { Link } from "react-router-dom";
 
 
 const handleFiltered = (searchInput, filteredRestaruntList) => {
@@ -32,15 +33,19 @@ const [allFilteredRestaurant, setAllFilteredRestaurant] = useState([])
 
   async function handleFetch() {
     const response = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.37240&lng=78.43780&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const data = await response.json();
+    const result =
+      data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
    console.log(
-     data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    //  /data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
    );
-    setFilteredRestaruntList(data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    setFilteredRestaruntList(result)
+      // data?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    
     setAllFilteredRestaurant(
-      data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      result
     );
    
   }
@@ -66,19 +71,23 @@ const [allFilteredRestaurant, setAllFilteredRestaurant] = useState([])
               (
                 filteredRestaruntList.map((each) => {
                   return (
-                    <li key={each.info.id} className="card">
-                      <img
-                        className="image"
-                        src={`${Img_url}${each.info.cloudinaryImageId}`}
-                        alt=""
-                      />
-                      <h2 className="main-headings">{each.info.name}</h2>
-                      <h3 className="headings cuisines">
-                        {each.info.cuisines.join(",")}
-                      </h3>
-                      <h4 className="headings">Rating: {each.info.avgRating}</h4>
-                      <h5 className="headings">{each.info.locality}</h5>
-                    </li>
+                    <Link to={`/restaurant/${each.info.id}`} key={each.info.id}>
+                      <li className="card">
+                        <img
+                          className="image"
+                          src={`${Img_url}${each.info.cloudinaryImageId}`}
+                          alt=""
+                        />
+                        <h2 className="main-headings">{each.info.name}</h2>
+                        <h3 className="headings cuisines">
+                          {each.info.cuisines.join(",")}
+                        </h3>
+                        <h4 className="headings">
+                          Rating: {each.info.avgRating}
+                        </h4>
+                        <h5 className="headings">{each.info.locality}</h5>
+                      </li>
+                    </Link>
                   );
                 })
               ) : <h1>Not found what your trying to search...</h1>}
--- src/components/RestaurantMenu/RestaurantMenu.js
+++ src/components/RestaurantMenu/RestaurantMenu.js
@@ -4,67 +4,116 @@ import { useParams } from "react-router-dom";
 import { Img_url } from "../Contents";
 
 const RestaurantMenu = () => {
-  const [restaurant, setRestaurant] = useState([]);
+  const [restaurant, setRestaurant] = useState(null);
   const [title,setTitle] = useState("")
   const { id } = useParams();
   useEffect(() => {
     getRestaurant()
     
 
-  },[])
+  },[id])
   // console.log(restaurant)
   async function getRestaurant() {
     try {
       const response = await fetch(
-        `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.406498&lng=78.47724389999999&restaurantId=${id}&catalog_qa=undefined&submitAction=ENTER`
+        `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=${id}&catalog_qa=undefined&submitAction=ENTER`
       );
       const data = await response.json()
-    
-      const json =
-        data?.data?.cards[4].groupedCard?.cardGroupMap?.REGULAR?.cards || [];
-    
-      const menu = data.data?.cards[0]?.card?.card?.text || "Menu";
-      setRestaurant(json);
-      setTitle(menu);
-      // console.log(data?.data?.cards[4].groupedCard?.cardGroupMap?.REGULAR?.cards);\
+      // console.log(response);
+      
+      setRestaurant(data?.data);
+      
+      setTitle(data?.data?.cards[0]?.card?.card?.text || "No Tittle Available");
+      // console.log(data?.data?.cards[4].groupedCard?.cardGroupMap?.REGULAR?.cards);
     }
     catch(error) {
       console.log('Failed to Fetch data',error)
     }
   }
-  // console.log(title);
-  console.log(restaurant);
-    return (
-      <>
-        <h1>RestaurantMenu Page,{id}</h1>
-        <h2>{title}</h2>
-        
-        {
-          restaurant.map((each,index) => {
-          return (
-            <div key={index}>
-              <h1>{each.card?.card?.title}</h1>
-              {each.card?.card?.itemCards.length > 0?
-              <div>{each.card?.card?.itemCards?.map((items,itemIndex) => {
-                return (
-                  // eslint-disable-next-line jsx-a11y/alt-text
-                  <>
-                  
-                  {items.card.info?.imageId && (<img
-                    key={itemIndex}
-                    src={`${Img_url}/${items.card.info?.imageId}`}
-                    />)}
-                    </>
-                );
+  // console.log(restaurant);  
+  if (!restaurant) {
+    return <h1>Loading...</h1>
+  }
+  const mainCard = restaurant.cards[2].card.card.info;
+  const reCommendedCards =
+    restaurant?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards;
+  console.log(reCommendedCards)
+  // console.log(mainCard)
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        gap: "14px",
+      }}
+    >
+      <h1>RestaurantMenu Page,{id}</h1>
+      <div style={{ display: "flex", alignItems: "flex-start" }}>
+        <h2 style={{ fontSize: "32px" }}>{title}</h2>
+      </div>
 
-              })}</div>
-              
-              :(each.card?.card?.categories?.itemCards)
-            }
-        }
-          }
-        
-      </>
-    );
+      <div
+        style={{
+          fontSize: "18px",
+          border: "1px solid #dfdfdf",
+          width: "50%",
+          borderRadius: "24px",
+          padding: "10px",
+          boxShadow: "0px 10px 8px 10px #e4e5ea",
+        }}
+      >
+        <ul
+          style={{
+            display: "flex",
+            gap: "15px",
+            alignItems: "center",
+            padding: "0px",
+          }}
+        >
+          <li style={{ listStyleType: "none" }}>
+            <span>{mainCard.avgRating}</span>({" "}
+            <span>{mainCard.totalRatingsString}</span> )
+          </li>
+          <li style={{ padding: "0px", listStyleType: "none" }}>
+            {mainCard.costForTwoMessage}
+          </li>
+        </ul>
+        <div
+          style={{
+            color: "#d7733f",
+            fontSize: "24px",
+            fontWeight: "bold",
+            display: "flex",
+            alignItems: "center",
+            gap: "5px",
+          }}
+        >
+          {mainCard.cuisines.map((cusin) => {
+            return (
+              <span style={{ textDecoration: "underline" }}>{cusin} </span>
+            );
+          })}
+        </div>
+
+        <p>{mainCard.locality}</p>
+        <p>{mainCard.sla.slaString}</p>
+      </div>
+
+      {
+        reCommendedCards.map(card => {
+          return (
+            <div key={card.card.info.id}>
+              <h1>{card.card.info.name}</h1>
+              <p>{card.card.info.description}</p>
+              <p>{card.card.info.price}</p>
+              <img src={`${Img_url}${card.card.info.imageId}`} />
+            </div>
+          );
+        })
+      }
+    </div>
+  );
 }
 export default RestaurantMenu;
